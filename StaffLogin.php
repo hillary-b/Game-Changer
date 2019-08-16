493dfb18b1ea625f39682da376a41a78cfa52443
hillary-b <31204891+hillary-b@users.noreply.github.com>
2019-08-16 09:46:28 +0100

    Create StaffLogin.php

---
+++ StaffLogin.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+  <title>Todo List</title>
+</head>
+
+<body>
+  <table>
+    <tr>
+    <th>Username: <input type="text" name="username" value="Username"></th>
+    <th>Password: <input type="text" name="password" value="Password"></th>
+    <th><input type="submit" value="Logint"></th>
+    </tr>
+  </table>
+</body>
+</html>
