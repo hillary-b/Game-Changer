edf0ff831f9bb314c1bb281a5042047f0e86f472
hillary-b <31204891+hillary-b@users.noreply.github.com>
2019-08-16 09:33:48 +0100

    Update Index.php

--- Index.php
+++ Index.php
@@ -2,12 +2,6 @@
 <html>
 <body>
 
-<?php
-echo "Click on what service you are looking for:";
-?><br>
-
-<input type="submit" value="Event Management">
-<input type="submit" value="Inspiration">
 
 </body>
 </html>
