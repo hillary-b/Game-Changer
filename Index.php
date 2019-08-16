560c31107d3f9eec4b01ecbb2a0f12bcd5deb6e8
hillary-b <31204891+hillary-b@users.noreply.github.com>
2019-08-16 09:59:11 +0100

    Update Index.php

--- Index.php
+++ Index.php
@@ -2,6 +2,19 @@
 <html>
 <body>
 
-
+  <a href="CustomerRegister.html">Register</a>
+  <a href="CustomerLogin.html" onclick="myFunction()">Login</a>
+  
+  <script>
+    function myFunction() {
+      var popup = document.getElementById("myPopup");
+      popup.classList.toggle("show");
+    }
+  </script>
+  
+  <span class="popuptext" id="myPopup">
+    
+  </span>
+  
 </body>
 </html>
